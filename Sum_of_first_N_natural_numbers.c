02c5cf6d464f0adf4c04222d65940c8c162acaa9
yathamsridharreddy <147792360+yathamsridharreddy@users.noreply.github.com>
2023-12-10 21:35:27 +0530

    #include<stdio.h> int Nat(int); int main() {     int n;     scanf("%d",&n);    int res=Nat(n);     printf("%d",res); } int Nat(int n) {     int Nat;     Nat=(n*(n+1))/2;     return Nat; }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -4,11 +4,11 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int res=Nat(n);
+   int res=Nat(n);
     printf("%d",res);
 }
 int Nat(int n)
-{
+{   
     int Nat;
     Nat=(n*(n+1))/2;
     return Nat;
