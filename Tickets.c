4e46fd5025e7999d7ad16e199d6ce9fc42f1ad0c
yathamsridharreddy <147792360+yathamsridharreddy@users.noreply.github.com>
2023-12-10 21:32:44 +0530

    #include<stdio.h> int main() {     int x,c;     scanf("%d",&x);     c=x*4;     if(c<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,c;
+    scanf("%d",&x);
+    c=x*4;
+    if(c<=1000)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
